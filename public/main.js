c7700dbf4f69ab21bd4bb11cd60509453cf5f450
Esme Tranquilino <etranquilino07@gmail.com>
2024-02-19 23:05:58 -0800

    update deployment lab

--- public/main.js
+++ public/main.js
@@ -1,3 +1,7 @@
 document.querySelector("#button").addEventListener('click', () => {
   document.querySelector("#content").style.color = 'red';
 })
+
+document.querySelector("#pink-btn").addEventListener('click', () => {
+  document.querySelector("#content").style.color = 'pink';
+})
